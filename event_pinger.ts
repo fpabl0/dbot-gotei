84532ab8e810feb9c96a0556a672ca44959c6152
FPabl0 <f.pablo1@hotmail.com>
2024-02-18 16:02:43 -0500

    added event pinger

---
+++ event_pinger.ts
@@ -0,0 +1,43 @@
+import { Request, Response } from "express";
+import { discordRequest } from "./discord_utils";
+
+const PING_CHANNEL_ID = process.env.PING_CHANNEL_ID;
+const XPINGKEY = process.env.XPINGKEY;
+const CW_PING = "1205239838756835348";
+const BUG_PING = "1208547257562169444";
+const FISH_PING = "1208547310104092692";
+const DYNA_PING = "1208878139137196162";
+
+export const handleEventPinger = async (req: Request, res: Response) => {
+  if (req.headers["x-ping-key"] !== XPINGKEY) {
+    res.sendStatus(401);
+    return;
+  }
+  try {
+    const message = pingEventMessage(req.query["event"] as string ?? "");
+    await discordRequest(`channels/${PING_CHANNEL_ID}/messages`, {
+      method: "POST",
+      body: {
+        content: message,
+        allowed_mentions: {
+          roles: [CW_PING, BUG_PING, FISH_PING, DYNA_PING],
+        },
+      } as any,
+    });
+    res.sendStatus(200);
+  } catch (e) {
+    res.status(500).send({ error: e });
+  }
+};
+
+const pingEventMessage = (evName: string) => {
+  let ret = "";
+  switch (evName) {
+    case "cw": ret += `<@&${CW_PING}>`; break;
+    case "bug": ret += `<@&${BUG_PING}>`; break;
+    case "fish": ret += `<@&${FISH_PING}>`; break;
+    case "dyna": ret += `<@&${DYNA_PING}>`; break;
+    default: throw "Invalid event";
+  }
+  return ret + " in less than 5 min";
+};
